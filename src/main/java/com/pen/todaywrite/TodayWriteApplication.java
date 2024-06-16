46606dc5e3f498c817e5ee4fc3fdc70d0566d567
hheey <hheey4481@gmail.com>
2024-06-16 17:18:45 +0900

    Initial commit

---
+++ src/main/java/com/pen/todaywrite/TodayWriteApplication.java
@@ -0,0 +1,13 @@
+package com.pen.todaywrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayWriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pen/todaywrite/TodayWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pen.todaywrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayWriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
